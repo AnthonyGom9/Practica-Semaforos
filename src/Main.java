77e0b936a61036d27a3c1cd5dcd4162f78b41520
Anthony Gomez <79477732+AnthonyGom9@users.noreply.github.com>
2024-08-16 23:56:29 -0600

    fix: se modificó la lógica del archivo main, utilizado para la simulación de carreras

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,26 @@
+import issues.raceCondition.Contador;
+import issues.raceCondition.HiloContador;
 import threads.BinarySemaphore;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private int count = 0;
-    private final BinarySemaphore semaphore = new BinarySemaphore();
 
-    public void increment() {
-        try {
-            semaphore.acquire(); // Adquirir acceso exclusivo
-            int newCount = count + 1;
-            Thread.sleep(50); // Simular un trabajo que podría causar una condición de carrera
-            count = newCount; // Actualizar la variable compartida
-            System.out.println(Thread.currentThread().getName() + " incremented count to: " + count);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } finally {
-            semaphore.release(); // Liberar el acceso exclusivo
-        }
-    }
+    public static void main(String[] args) throws InterruptedException{
+        Contador contador = new Contador();
 
-    public static void main(String[] args) {
-        Main example = new Main();
+        Runnable tarea1 = new HiloContador(contador);
+        Runnable tarea2 = new HiloContador(contador);
 
-        Runnable task = example::increment;
+        Thread hilo1 = new Thread(tarea1);
+        Thread hilo2 = new Thread(tarea2);
 
-        Thread t1 = new Thread(task, "Thread 1");
-        Thread t2 = new Thread(task, "Thread 2");
+        hilo1.start();
+        hilo2.start();
 
-        t1.start();
-        t2.start();
+        hilo1.join();
+        hilo2.join();
 
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        System.out.println("Final count: " + example.count);
-
+        System.out.println("Valor final del contador: " + contador.getContador());
     }
 }
